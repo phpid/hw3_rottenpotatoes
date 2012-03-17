6b5bbb95cd4664f17f84dd509c1608573dd972e0
Vyacheslav Kvasnitsky <phpidev@yahoo.com>
2012-03-17 13:28:34 -0700

    hw-part1

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,12 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+  Movie.destroy_all
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.new(movie).save
   end
-  assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
